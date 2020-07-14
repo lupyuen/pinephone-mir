9fb0c9cf65fdfda158780c4509d95c523be07a72
lupyuen <luppy@appkaki.com>
2020-07-14 16:52:38 +0800

    Log file manager

--- shm.c
+++ shm.c
@@ -13,7 +13,6 @@
 #include <errno.h>
 #include <unistd.h>
 
-
 struct wl_display *display = NULL;
 struct wl_compositor *compositor = NULL;
 struct wl_surface *surface;
@@ -29,7 +28,7 @@ int HEIGHT = 360;
 
 static void
 handle_ping(void *data, struct wl_shell_surface *shell_surface,
-							uint32_t serial)
+            uint32_t serial)
 {
     wl_shell_surface_pong(shell_surface, serial);
     fprintf(stderr, "Pinged and ponged\n");
@@ -37,7 +36,7 @@ handle_ping(void *data, struct wl_shell_surface *shell_surface,
 
 static void
 handle_configure(void *data, struct wl_shell_surface *shell_surface,
-		 uint32_t edges, int32_t width, int32_t height)
+                 uint32_t edges, int32_t width, int32_t height)
 {
 }
 
@@ -47,51 +46,51 @@ handle_popup_done(void *data, struct wl_shell_surface *shell_surface)
 }
 
 static const struct wl_shell_surface_listener shell_surface_listener = {
-	handle_ping,
-	handle_configure,
-	handle_popup_done
-};
+    handle_ping,
+    handle_configure,
+    handle_popup_done};
 
 static int
 set_cloexec_or_close(int fd)
 {
-        long flags;
+    long flags;
 
-        if (fd == -1)
-                return -1;
+    if (fd == -1)
+        return -1;
 
-        flags = fcntl(fd, F_GETFD);
-        if (flags == -1)
-                goto err;
+    flags = fcntl(fd, F_GETFD);
+    if (flags == -1)
+        goto err;
 
-        if (fcntl(fd, F_SETFD, flags | FD_CLOEXEC) == -1)
-                goto err;
+    if (fcntl(fd, F_SETFD, flags | FD_CLOEXEC) == -1)
+        goto err;
 
-        return fd;
+    return fd;
 
 err:
-        close(fd);
-        return -1;
+    close(fd);
+    return -1;
 }
 
 static int
 create_tmpfile_cloexec(char *tmpname)
 {
-        int fd;
+    int fd;
 
 #ifdef HAVE_MKOSTEMP
-        fd = mkostemp(tmpname, O_CLOEXEC);
-        if (fd >= 0)
-                unlink(tmpname);
+    fd = mkostemp(tmpname, O_CLOEXEC);
+    if (fd >= 0)
+        unlink(tmpname);
 #else
-        fd = mkstemp(tmpname);
-        if (fd >= 0) {
-                fd = set_cloexec_or_close(fd);
-                unlink(tmpname);
-        }
+    fd = mkstemp(tmpname);
+    if (fd >= 0)
+    {
+        fd = set_cloexec_or_close(fd);
+        unlink(tmpname);
+    }
 #endif
 
-        return fd;
+    return fd;
 }
 
 /*
@@ -109,54 +108,59 @@ create_tmpfile_cloexec(char *tmpname)
  * transmitting the file descriptor over Unix sockets using the
  * SCM_RIGHTS methods.
  */
-int
-os_create_anonymous_file(off_t size)
+int os_create_anonymous_file(off_t size)
 {
-        static const char template[] = "/weston-shared-XXXXXX";
-        const char *path;
-        char *name;
-        int fd;
+    static const char template[] = "/weston-shared-XXXXXX";
+    const char *path;
+    char *name;
+    int fd;
 
-        path = getenv("XDG_RUNTIME_DIR");
-        if (!path) {
-                errno = ENOENT;
-                return -1;
-        }
+    path = getenv("XDG_RUNTIME_DIR");
+    if (!path)
+    {
+        errno = ENOENT;
+        return -1;
+    }
 
-        name = malloc(strlen(path) + sizeof(template));
-        if (!name)
-                return -1;
-        strcpy(name, path);
-        strcat(name, template);
+    name = malloc(strlen(path) + sizeof(template));
+    if (!name)
+        return -1;
+    strcpy(name, path);
+    strcat(name, template);
 
-        fd = create_tmpfile_cloexec(name);
+    printf("Creating temp file %s\n...", name);
+    fd = create_tmpfile_cloexec(name);
 
-        free(name);
+    free(name);
 
-        if (fd < 0)
-                return -1;
+    if (fd < 0)
+        return -1;
 
-        if (ftruncate(fd, size) < 0) {
-                close(fd);
-                return -1;
-        }
+    if (ftruncate(fd, size) < 0)
+    {
+        close(fd);
+        return -1;
+    }
 
-        return fd;
+    return fd;
 }
 
 static void
-paint_pixels() {
+paint_pixels()
+{
     int n;
     uint32_t *pixel = shm_data;
 
     fprintf(stderr, "Painting pixels\n");
-    for (n =0; n < WIDTH*HEIGHT; n++) {
-	*pixel++ = 0xffff;
+    for (n = 0; n < WIDTH * HEIGHT; n++)
+    {
+        *pixel++ = 0xffff;
     }
 }
 
 static struct wl_buffer *
-create_buffer() {
+create_buffer()
+{
     struct wl_shm_pool *pool;
     int stride = WIDTH * 4; // 4 bytes per pixel
     int size = stride * HEIGHT;
@@ -164,31 +168,34 @@ create_buffer() {
     struct wl_buffer *buff;
 
     fd = os_create_anonymous_file(size);
-    if (fd < 0) {
-	fprintf(stderr, "creating a buffer file for %d B failed: %m\n",
-		size);
-	exit(1);
+    if (fd < 0)
+    {
+        fprintf(stderr, "creating a buffer file for %d B failed: %m\n",
+                size);
+        exit(1);
     }
-    
+
     shm_data = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    if (shm_data == MAP_FAILED) {
-	fprintf(stderr, "mmap failed: %m\n");
-	close(fd);
-	exit(1);
+    if (shm_data == MAP_FAILED)
+    {
+        fprintf(stderr, "mmap failed: %m\n");
+        close(fd);
+        exit(1);
     }
 
     pool = wl_shm_create_pool(shm, fd, size);
     buff = wl_shm_pool_create_buffer(pool, 0,
-					  WIDTH, HEIGHT,
-					  stride, 	
-					  WL_SHM_FORMAT_XRGB8888);
+                                     WIDTH, HEIGHT,
+                                     stride,
+                                     WL_SHM_FORMAT_XRGB8888);
     //wl_buffer_add_listener(buffer, &buffer_listener, buffer);
     wl_shm_pool_destroy(pool);
     return buff;
 }
 
 static void
-create_window() {
+create_window()
+{
 
     buffer = create_buffer();
 
@@ -207,26 +214,29 @@ shm_format(void *data, struct wl_shm *wl_shm, uint32_t format)
 }
 
 struct wl_shm_listener shm_listener = {
-	shm_format
-};
+    shm_format};
 
 static void
 global_registry_handler(void *data, struct wl_registry *registry, uint32_t id,
-	       const char *interface, uint32_t version)
+                        const char *interface, uint32_t version)
 {
-    if (strcmp(interface, "wl_compositor") == 0) {
-        compositor = wl_registry_bind(registry, 
-				      id, 
-				      &wl_compositor_interface, 
-				      1);
-    } else if (strcmp(interface, "wl_shell") == 0) {
+    if (strcmp(interface, "wl_compositor") == 0)
+    {
+        compositor = wl_registry_bind(registry,
+                                      id,
+                                      &wl_compositor_interface,
+                                      1);
+    }
+    else if (strcmp(interface, "wl_shell") == 0)
+    {
         shell = wl_registry_bind(registry, id,
                                  &wl_shell_interface, 1);
-    } else if (strcmp(interface, "wl_shm") == 0) {
+    }
+    else if (strcmp(interface, "wl_shm") == 0)
+    {
         shm = wl_registry_bind(registry, id,
-                                 &wl_shm_interface, 1);
-	wl_shm_add_listener(shm, &shm_listener, NULL);
-       
+                               &wl_shm_interface, 1);
+        wl_shm_add_listener(shm, &shm_listener, NULL);
     }
 }
 
@@ -238,16 +248,16 @@ global_registry_remover(void *data, struct wl_registry *registry, uint32_t id)
 
 static const struct wl_registry_listener registry_listener = {
     global_registry_handler,
-    global_registry_remover
-};
+    global_registry_remover};
 
-
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 
     display = wl_display_connect(NULL);
-    if (display == NULL) {
-	fprintf(stderr, "Can't connect to display\n");
-	exit(1);
+    if (display == NULL)
+    {
+        fprintf(stderr, "Can't connect to display\n");
+        exit(1);
     }
     printf("connected to display\n");
 
@@ -257,43 +267,52 @@ int main(int argc, char **argv) {
     wl_display_dispatch(display);
     wl_display_roundtrip(display);
 
-    if (compositor == NULL) {
-	fprintf(stderr, "Can't find compositor\n");
-	exit(1);
-    } else {
-	fprintf(stderr, "Found compositor\n");
+    if (compositor == NULL)
+    {
+        fprintf(stderr, "Can't find compositor\n");
+        exit(1);
+    }
+    else
+    {
+        fprintf(stderr, "Found compositor\n");
     }
 
     surface = wl_compositor_create_surface(compositor);
-    if (surface == NULL) {
-	fprintf(stderr, "Can't create surface\n");
-	exit(1);
-    } else {
-	fprintf(stderr, "Created surface\n");
+    if (surface == NULL)
+    {
+        fprintf(stderr, "Can't create surface\n");
+        exit(1);
+    }
+    else
+    {
+        fprintf(stderr, "Created surface\n");
     }
 
     shell_surface = wl_shell_get_shell_surface(shell, surface);
-    if (shell_surface == NULL) {
-	fprintf(stderr, "Can't create shell surface\n");
-	exit(1);
-    } else {
-	fprintf(stderr, "Created shell surface\n");
+    if (shell_surface == NULL)
+    {
+        fprintf(stderr, "Can't create shell surface\n");
+        exit(1);
+    }
+    else
+    {
+        fprintf(stderr, "Created shell surface\n");
     }
     wl_shell_surface_set_toplevel(shell_surface);
 
     wl_shell_surface_add_listener(shell_surface,
-				  &shell_surface_listener, NULL);
-
+                                  &shell_surface_listener, NULL);
 
     create_window();
     paint_pixels();
 
-    while (wl_display_dispatch(display) != -1) {
-	;
+    while (wl_display_dispatch(display) != -1)
+    {
+        ;
     }
 
     wl_display_disconnect(display);
     printf("disconnected from display\n");
-    
+
     exit(0);
 }
