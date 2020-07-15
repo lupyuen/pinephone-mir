427e005ab0817d62d23549645d217e7a1225df68
lupyuen <luppy@appkaki.com>
2020-07-15 16:42:51 +0800

    Test

--- shm.c
+++ shm.c
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 #include <wayland-client.h>
 #include <wayland-client-protocol.h>
 #include <wayland-server-protocol.h>
