62badc0da35ffd34e7cc507fa0f3661a07cf222e
Lee Lup Yuen <luppy@appkaki.com>
2020-07-20 19:55:16 +0800

    Clean up

--- egl.c
+++ egl.c
@@ -12,25 +12,39 @@
 #include <wayland-egl.h>
 #include <EGL/egl.h>
 #include <GLES2/gl2.h>
+#include "util.h"
+
+/// Exported from texture.c
+void Init(ESContext *esContext);
+void Draw(ESContext *esContext);
+
+/// Local Functions
+static void get_server_references(void);
+static void create_opaque_region(void);
+static void create_window(void);
+static void init_egl(void);
 
 /// Dimensions of the OpenGL region to be rendered
-int WIDTH  = 480;
-int HEIGHT = 360;
+static int WIDTH  = 480;
+static int HEIGHT = 360;
 
 /// Wayland Interfaces
-struct wl_display       *display;       //  Wayland Display
-struct wl_compositor    *compositor;    //  Wayland Compositor
-struct wl_surface       *surface;       //  Wayland Surface
-struct wl_egl_window    *egl_window;    //  Wayland EGL Window
-struct wl_region        *region;        //  Wayland Region
-struct wl_shell         *shell;         //  Wayland Shell
-struct wl_shell_surface *shell_surface; //  Wayland Shell Surface
+static struct wl_display       *display;       //  Wayland Display
+static struct wl_compositor    *compositor;    //  Wayland Compositor
+static struct wl_surface       *surface;       //  Wayland Surface
+static struct wl_egl_window    *egl_window;    //  Wayland EGL Window
+static struct wl_region        *region;        //  Wayland Region
+static struct wl_shell         *shell;         //  Wayland Shell
+static struct wl_shell_surface *shell_surface; //  Wayland Shell Surface
 
 /// Wayland EGL Interfaces for OpenGL Rendering
-EGLDisplay egl_display;  //  EGL Display
-EGLConfig  egl_conf;     //  EGL Configuration
-EGLSurface egl_surface;  //  EGL Surface
-EGLContext egl_context;  //  EGL Context
+static EGLDisplay egl_display;  //  EGL Display
+static EGLConfig  egl_conf;     //  EGL Configuration
+static EGLSurface egl_surface;  //  EGL Surface
+static EGLContext egl_context;  //  EGL Context
+
+////////////////////////////////////////////////////////////////////
+//  Render OpenGL
 
 /// Render the OpenGL ES2 display
 static void render_display() {
@@ -47,6 +61,84 @@ static void render_display() {
 
     // Render now
     glFlush();
+}
+
+////////////////////////////////////////////////////////////////////
+//  Main
+
+/// Connect to Wayland Compositor and render OpenGL graphics
+int main(int argc, char **argv) {
+    //  Get interfaces for Wayland Compositor and Wayland Shell
+    get_server_references();
+    assert(compositor != NULL);  //  Failed to get Wayland Compositor
+    assert(shell != NULL);       //  Failed to get Wayland Shell
+
+    //  Create a Wayland Surface for rendering
+    surface = wl_compositor_create_surface(compositor);
+    assert(surface != NULL);  //  Failed to create Wayland Surface
+
+    //  Get the Wayland Shell Surface for rendering
+    shell_surface = wl_shell_get_shell_surface(shell, surface);
+    assert(shell_surface != NULL);
+
+    //  Set the Shell Surface as top level
+    wl_shell_surface_set_toplevel(shell_surface);
+
+    //  Create an opaque region for rendering
+    create_opaque_region();
+
+    //  Init Wayland EGL
+    init_egl();
+
+    //  Create the OpenGL Window and render OpenGL graphics
+    create_window();
+
+    //  Handle all Wayland Events in the Event Loop
+    while (wl_display_dispatch(display) != -1) {}
+
+    //  Disconnect from the Wayland Display
+    wl_display_disconnect(display);
+    puts("Disconnected from display");
+    return 0;
+}
+
+////////////////////////////////////////////////////////////////////
+//  Wayland Registry
+
+static void global_registry_handler(void *data, struct wl_registry *registry, uint32_t id,
+    const char *interface, uint32_t version);
+static void global_registry_remover(void *data, struct wl_registry *registry, uint32_t id);
+
+/// Callbacks for interfaces returned by Wayland Service
+static const struct wl_registry_listener registry_listener = {
+    global_registry_handler,
+    global_registry_remover
+};
+
+/// Connect to Wayland Service and fetch the interfaces for Wayland Compositor and Wayland Shell
+static void get_server_references(void) {
+    puts("Getting server references...");
+    display = wl_display_connect(NULL);
+    if (display == NULL) {
+        fprintf(stderr, "Failed to connect to display\n");
+        exit(1);
+    }
+    puts("Connected to display");
+
+    //  Get the Wayland Registry
+    struct wl_registry *registry = wl_display_get_registry(display);
+    assert(registry != NULL);  //  Failed to get Wayland Registry
+
+    //  Add Registry Callbacks to handle interfaces returned by Wayland Service
+    wl_registry_add_listener(registry, &registry_listener, NULL);
+
+    //  Wait for Registry Callbacks to fetch Wayland Interfaces
+    wl_display_dispatch(display);
+    wl_display_roundtrip(display);
+
+    //  We should have received interfaces for Wayland Compositor and Wayland Shell
+    assert(compositor != NULL);  //  Failed to get Wayland Compositor
+    assert(shell != NULL);       //  Failed to get Wayland Shell
 }
 
 /// Callback for interfaces returned by Wayland Service
@@ -69,205 +161,96 @@ static void global_registry_handler(void *data, struct wl_registry *registry, ui
 
 /// Callback for removed interfaces
 static void global_registry_remover(void *data, struct wl_registry *registry, uint32_t id) {
-    printf("Removed id %d\n", id);
+    printf("Removed interface id %d\n", id);
 }
 
-/// Callbacks for interfaces returned by Wayland Service
-static const struct wl_registry_listener registry_listener = {
-    global_registry_handler,
-    global_registry_remover
-};
+////////////////////////////////////////////////////////////////////
+//  Wayland EGL
 
 /// Create an opaque region for OpenGL rendering
-static void create_opaque_region() {
+static void create_opaque_region(void) {
     puts("Creating opaque region...");
     region = wl_compositor_create_region(compositor);
-    assert(region != NULL);
+    assert(region != NULL);  //  Failed to create EGL Region
 
     wl_region_add(region, 0, 0, WIDTH, HEIGHT);
     wl_surface_set_opaque_region(surface, region);
 }
 
 /// Create the OpenGL window and render it
-static void create_window() {
+static void create_window(void) {
     //  Create an OpenGL Window
     egl_window = wl_egl_window_create(surface, WIDTH, HEIGHT);
-    assert(egl_window != EGL_NO_SURFACE);
+    assert(egl_window != EGL_NO_SURFACE);  //  Failed to create OpenGL Window
 
     //  Create an OpenGL Window Surface for rendering
     egl_surface = eglCreateWindowSurface(egl_display, egl_conf,
         egl_window, NULL);
-    assert(egl_surface != NULL);
+    assert(egl_surface != NULL);  //  Failed to create OpenGL Window Surface
 
     //  Set the current rendering surface
     EGLBoolean madeCurrent = eglMakeCurrent(egl_display, egl_surface,
         egl_surface, egl_context);
-    assert(madeCurrent);
+    assert(madeCurrent);  //  Failed to set rendering surface
 
     //  Render the display
     render_display();
 
     //  Swap the display buffers to make the display visible
     EGLBoolean swappedBuffers = eglSwapBuffers(egl_display, egl_surface);
-    assert(swappedBuffers);
+    assert(swappedBuffers);  //  Failed to swap display buffers
 }
 
-static void
-init_egl()
-{
-    EGLint major, minor, count, n, size;
-    EGLConfig *configs;
-    int i;
-    EGLint config_attribs[] = {
-        EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
-        EGL_RED_SIZE, 8,
-        EGL_GREEN_SIZE, 8,
-        EGL_BLUE_SIZE, 8,
-        EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT,
-        EGL_NONE};
+/// Init the EGL Interface
+static void init_egl(void) {
+    puts("Init EGL...");
 
+    //  Attributes for our EGL Display
+    EGLint config_attribs[] = {
+        EGL_SURFACE_TYPE,    EGL_WINDOW_BIT,
+        EGL_RED_SIZE,        8,
+        EGL_GREEN_SIZE,      8,
+        EGL_BLUE_SIZE,       8,
+        EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT,
+        EGL_NONE
+    };
     static const EGLint context_attribs[] = {
         EGL_CONTEXT_CLIENT_VERSION, 2,
-        EGL_NONE};
+        EGL_NONE
+    };
 
-    egl_display = eglGetDisplay((EGLNativeDisplayType)display);
-    if (egl_display == EGL_NO_DISPLAY)
-    {
-        fprintf(stderr, "Can't create egl display\n");
-        exit(1);
-    }
-    else
-    {
-        fprintf(stderr, "Created egl display\n");
-    }
+    //  Get the EGL Display
+    egl_display = eglGetDisplay((EGLNativeDisplayType) display);
+    assert(egl_display != EGL_NO_DISPLAY);  //  Failed to get EGL Display
 
-    if (eglInitialize(egl_display, &major, &minor) != EGL_TRUE)
-    {
-        fprintf(stderr, "Can't initialise egl display\n");
-        exit(1);
-    }
+    //  Init the EGL Display
+    EGLint major, minor;
+    EGLBoolean egl_init = eglInitialize(egl_display, &major, &minor);
+    assert(egl_init);  //  Failed to init EGL Display
     printf("EGL major: %d, minor %d\n", major, minor);
 
+    //  Get the EGL Configurations
+    EGLint count, n;
     eglGetConfigs(egl_display, NULL, 0, &count);
     printf("EGL has %d configs\n", count);
-
-    configs = calloc(count, sizeof *configs);
-
+    EGLConfig *configs = calloc(count, sizeof *configs);
     eglChooseConfig(egl_display, config_attribs,
-                    configs, count, &n);
+        configs, count, &n);
 
-    for (i = 0; i < n; i++)
-    {
-        eglGetConfigAttrib(egl_display,
-                           configs[i], EGL_BUFFER_SIZE, &size);
+    //  Choose the first EGL Configuration
+    for (int i = 0; i < n; i++) {
+        EGLint size;
+        eglGetConfigAttrib(egl_display, configs[i], EGL_BUFFER_SIZE, &size);
         printf("Buffer size for config %d is %d\n", i, size);
-        eglGetConfigAttrib(egl_display,
-                           configs[i], EGL_RED_SIZE, &size);
+        eglGetConfigAttrib(egl_display, configs[i], EGL_RED_SIZE, &size);
         printf("Red size for config %d is %d\n", i, size);
-
-        // just choose the first one
         egl_conf = configs[i];
         break;
     }
+    assert(egl_conf != NULL);  //  Failed to get EGL Configuration
 
-    egl_context =
-        eglCreateContext(egl_display,
-                         egl_conf,
-                         EGL_NO_CONTEXT, context_attribs);
+    //  Create the EGL Context based on the EGL Display and Configuration
+    egl_context = eglCreateContext(egl_display, egl_conf,
+        EGL_NO_CONTEXT, context_attribs);
+    assert(egl_context != NULL);  //  Failed to create EGL Context
 }
-
-static void
-get_server_references(void)
-{
-
-    display = wl_display_connect(NULL);
-    if (display == NULL)
-    {
-        fprintf(stderr, "Can't connect to display\n");
-        exit(1);
-    }
-    printf("connected to display\n");
-
-    struct wl_registry *registry = wl_display_get_registry(display);
-    wl_registry_add_listener(registry, &registry_listener, NULL);
-
-    wl_display_dispatch(display);
-    wl_display_roundtrip(display);
-
-    if (compositor == NULL || shell == NULL)
-    {
-        fprintf(stderr, "Can't find compositor or shell\n");
-        exit(1);
-    }
-    else
-    {
-        fprintf(stderr, "Found compositor and shell\n");
-    }
-}
-
-int main(int argc, char **argv)
-{
-
-    get_server_references();
-
-    surface = wl_compositor_create_surface(compositor);
-    if (surface == NULL)
-    {
-        fprintf(stderr, "Can't create surface\n");
-        exit(1);
-    }
-    else
-    {
-        fprintf(stderr, "Created surface\n");
-    }
-
-    shell_surface = wl_shell_get_shell_surface(shell, surface);
-    wl_shell_surface_set_toplevel(shell_surface);
-
-    create_opaque_region();
-    init_egl();
-    create_window();
-
-    while (wl_display_dispatch(display) != -1)
-    {
-        ;
-    }
-
-    wl_display_disconnect(display);
-    printf("disconnected from display\n");
-
-    exit(0);
-}
-
-/* Output:
-++ gcc -o egl egl.c -lwayland-client -lwayland-server -lwayland-egl -L/usr/lib/aarch64-linux-gnu/mesa-egl -lEGL /usr/lib/aarch64-linux-gnu/mesa-egl/libGLESv2.so.2 -Wl,-Map=egl.map
-++ sudo mount -o remount,rw /
-++ sudo cp egl /usr/share/click/preinstalled/.click/users/@all/com.ubuntu.camera/camera-app
-++ ls -l /usr/share/click/preinstalled/.click/users/@all/com.ubuntu.camera/camera-app
--rwxr-xr-x 1 clickpkg clickpkg 20568 Jul 12 03:27 /usr/share/click/preinstalled/.click/users/@all/com.ubuntu.camera/camera-app
-++ echo
-++ tail -f /home/phablet/.cache/upstart/application-click-com.ubuntu.camera_camera_3.1.3.log
-
-connected to display
-Got a registry event for wl_drm id 1
-Got a registry event for qt_windowmanager id 2
-Got a registry event for wl_compositor id 3
-Got a registry event for wl_subcompositor id 4
-Got a registry event for wl_seat id 5
-Got a registry event for wl_output id 6
-Got a registry event for wl_data_device_manager id 7
-Got a registry event for wl_shell id 8
-Got a registry event for zxdg_shell_v6 id 9
-Got a registry event for xdg_wm_base id 10
-Got a registry event for wl_shm id 11
-Found compositor and shell
-Created surface
-Created egl display
-EGL major: 1, minor 4
-EGL has 14 configs
-Buffer size for config 0 is 24
-Red size for config 0 is 8
-Created egl window
-Made current
-Swapped buffers
-*/
